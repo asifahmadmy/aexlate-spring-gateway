8c1bd9841a43b9a92b7ad3615de7bc48b24a76b1
asifahmadmy <asif-ahmad@hotmail.com>
2023-03-06 18:47:44 +0800

    First commit

---
+++ src/test/java/com/aexlate/gateway/AexlateSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aexlate.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AexlateSpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
